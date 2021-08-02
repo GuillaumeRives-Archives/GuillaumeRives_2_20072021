c39583379c3c2c00cff4b7973acfa6d54c093c52
Guillaume Rives <rives.guillaume@gmail.com>
2021-08-02 18:32:02 +0200

    Suppression du fichier JS Comme vu avec l'examinateur

--- js/activeLinks.js
+++
@@ -1,9 +0,0 @@
-const activeLinks = document.querySelectorAll(".anchor");
-
-activeLinks.forEach(function(link){
-    link.addEventListener("click", event => {
-        activeLinks.forEach(function(link){
-            link.classList.toggle("active");
-        });
-    });
-});
